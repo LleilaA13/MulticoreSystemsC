8f431fb37918dc86645d4c1044ad2e1e7ffb975f
LleilaA13 <zanoni.2033176@studenti.uniroma1.it>
2024-10-11 20:20:42 +0200

    exercises, catching up

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 # Variables
 CC = mpicc                      # Use mpicc for MPI programs
 CFLAGS = -Wall -g 			     # Compiler flags
-LDFLAGS = #-lm                       # Add any additional libraries here (if needed)
-SRC = lec6_2.c     # Source files
+LDFLAGS = -lm                       # Add any additional libraries here (if needed)
+SRC = multicore-programming/lec5/ring.c     # Source files
 OBJ = $(SRC:.c=.o)               # Object files
-TARGET = lec6        # Name of the final executable
+TARGET = ring        # Name of the final executable
 #TOSSES = 1000000     # Number of tosses for the Monte Carlo simulation
 # Number of processes (for running the MPI program, not for compiling)
 NP = 4                           # Default number of processes
---
+++ c-practice/arrays.c
@@ -0,0 +1,87 @@
+#include <stdio.h> 
+//Esempio array in C:
+/*
+int main(){
+    
+    int dim = 100;
+    int array[dim];
+
+    int i = 0;
+
+    for(i = 0; i < dim; i++){
+        array[i] = i;
+    }
+
+    for(i = 0; i < dim; i++){
+        printf("%d \n", array[i]);
+    }
+
+    return 0;
+
+}
+*/
+
+
+//esercizio 4:
+/* Scriva un programma che legge in input 5 numeri interi e li salva in un array:
+    1) Il programma stampa il numero più grande e il numero più piccolo.
+    2) Il programma stampa la somma di tutti i numeri.
+
+        N.B.: per svolgere l'esercizio si deve utilizzare un array
+            e cicli FOR
+
+
+int main(){
+    int array[5];
+    int i = 0;
+
+    for(i = 0; i < 5; i++){
+        printf("Inserire numero: %d \n", i);
+        scanf("%d", &array[i]);
+    }
+
+    int min = array[0];
+    int max = array[0];
+    int sum = 0;
+
+    for(i = 0; i < 5; i++){
+        if(array[i] < min)
+            min = array[i];
+        if(array[i] > max)
+            max = array[i];
+        
+        sum += array[i];
+    }
+
+    printf("minimo: %d -  massimo: %d - somma: %d  \n", min, max ,sum);
+    
+    return 0;
+
+}
+*/
+
+//Array multidimensionali:
+
+int main(){
+
+    int matrice[2][3];
+
+    int riga = 0;
+    int colonna = 0;
+
+    for (riga = 0; riga < 2; riga++){
+        for (colonna = 0; colonna < 3; colonna ++){
+            printf("Input [%d][%d] \n", riga, colonna);
+            scanf("%d", &matrice[riga][colonna]);
+        }
+    }
+
+    for (riga = 0; riga < 2; riga++){
+        for (colonna = 0; colonna < 3; colonna ++){
+            printf("%d", matrice[riga][colonna]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
